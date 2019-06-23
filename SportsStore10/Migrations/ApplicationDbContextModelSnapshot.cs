5b9d890f0f6787dfa38a75828a9bda13c38ca27f
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-06-23 13:10:25 -0600

    Added Orders page

--- SportsStore10/Migrations/ApplicationDbContextModelSnapshot.cs
+++ SportsStore10/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace SportsStore.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
+                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -63,6 +63,8 @@ namespace SportsStore.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired();
+
+                    b.Property<bool>("Shipped");
 
                     b.Property<string>("State")
                         .IsRequired();
